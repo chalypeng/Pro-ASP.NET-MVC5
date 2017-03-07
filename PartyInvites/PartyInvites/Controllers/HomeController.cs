17545b0ae6d5dcf931c1025134c0f5d424396bfc
chalypeng <chalypeng@gmail.com>
2017-03-07 19:45:48 +0800

    update files

---
+++ PartyInvites/PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using PartyInvites.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View();
+        }
+
+        //public string Index()
+        //{
+        //    return "Hello World";
+        //}
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                //  给晚会组织发送邮件
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                // 有验证错误
+                return View();
+            }
+            
+        }
+	}
+}
