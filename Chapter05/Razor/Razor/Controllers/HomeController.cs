f3ae7277d819f56d686fba44983727d939359d87
chalypeng <chalypeng@gmail.com>
2017-04-01 10:46:18 +0800

    finish chapter 5...

--- Chapter05/Razor/Razor/Controllers/HomeController.cs
+++ Chapter05/Razor/Razor/Controllers/HomeController.cs
@@ -32,5 +32,17 @@ namespace Razor.Controllers {
 
             return View(myProduct);
         }
+        
+        public ActionResult DemoArray()
+        {
+            Product[] array = {
+                new Product{ Name="Kayak", Price=275M},
+                new Product{ Name="Lifejacket", Price=48.5M},
+                new Product{ Name="Soccer ball", Price=19.50M},
+                new Product{ Name="Corner flag", Price=34.95M}          
+            };
+            return View(array);
+        }
+
     }
 }
