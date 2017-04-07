a35b0b45a5637f29bebc0c3ac6c6b50a37514e51
chalypeng <chalypeng@gmail.com>
2017-04-07 16:11:47 +0800

    done chp10..

--- Chapter07/SportsStore/SportsStore.WebUI/Controllers/NavController.cs
+++ Chapter07/SportsStore/SportsStore.WebUI/Controllers/NavController.cs
@@ -13,14 +13,14 @@ namespace SportsStore.WebUI.Controllers
         public NavController(IProductRepository repo) {
             repository = repo;
         }
-        public PartialViewResult Menu(string category=null,bool horizontalLayout=false) {
+        public PartialViewResult Menu(string category=null) {
             ViewBag.SelectedCategory = category;
             IEnumerable<string> categories = repository.Products
                 .Select(x => x.Category)
                 .Distinct()
                 .OrderBy(x => x);
-            string viewName = horizontalLayout ? "MenuHorizontal" : "Menu";
-            return PartialView(viewName,categories);
+            
+            return PartialView("FlexMenu",categories);
 
         }
     }
